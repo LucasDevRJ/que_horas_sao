d5f1fabc3638f1c64affc2fbad065c352ece23af
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-19 21:22:32 -0300

    Armazenando o horário atual

--- javaScript/relogio.js
+++ javaScript/relogio.js
@@ -1,3 +1,10 @@
+//Armazenando o horário atual
+const horarioAtual = new Date();
+const horarioAtualLocal = horarioAtual.toLocaleTimeString();
+
+console.log(horarioAtualLocal);
+
+//Armazenando a data atual
 const dataAtual = new Date();
 const dataAtualLocal = dataAtual.toLocaleDateString();
 
